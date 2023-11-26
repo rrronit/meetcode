2205644d5cf15d3dc362753f59735c9851065117
rrronit <ronitsingh.cool4@gmail.com>
2023-11-27 03:27:50 +0530

    added vercel.json

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  base:"/",
-  plugins: [react()],
-})
+  base: '/', 
+  build: {
+    outDir: 'dist',
+  },
+  plugins:[react()]
+});
